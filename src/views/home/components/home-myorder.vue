f183990fff4ed2cd68bb0e9ffd405995ce12baeb
aceu <1950163205@qq.com>
2022-09-29 22:39:31 +0800

    跑腿2

---
+++ src/views/home/components/home-myorder.vue
@@ -0,0 +1,62 @@
+<template>
+<div>
+<div class="tableflex">
+    <table class="ui celled padded table">
+    <thead>
+        <tr><th class="single line">订单状态</th>
+        <th>订单金额</th>
+        <th>订单类型</th>
+        <th>订单地址</th>
+        <th>订单时间</th>
+        <th>订单内容</th>
+        <th>操作</th>
+    </tr></thead>
+    <tbody>
+        <tr>
+        <td>
+            <h2 class="ui center aligned header">A</h2>
+        </td>
+        <td class="single line">电源输出 </td>
+        <td>
+            <div class="ui star rating" data-rating="3" data-max-rating="3"></div>
+        </td>
+        <td>a</td>
+        <td class="right aligned">提交:<br> 结束: </td>
+        <td>Creatine  is variability in this increase, however, with some nonrespoders.</td>
+        <td>啊</td>
+        </tr>
+        <tr>
+        <td>
+            <h2 class="ui center aligned header">A</h2>
+        </td>
+        <td class="single line">重量 </td>
+        <td>
+            <div class="ui star rating" data-rating="3" data-max-rating="3"></div>
+        </td>
+        <td>a</td>
+        <td class="right aligned">提交:<br> 结束: </td>
+        <td>Creatine is the referenc assess potency</td>
+        <td>啊</td>
+        </tr>
+    </tbody>
+    </table>
+</div>
+</div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+.ui.celled.padded.table{
+    width: 1200px;
+}
+.tableflex{
+    display: flex;
+    justify-content: center;
+}
+</style>
